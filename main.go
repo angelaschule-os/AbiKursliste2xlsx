e71016a8fc2ae19cb03ce8b6b55cf0c1ac4b19d8
Bjoern Schilberg <bjoern.schilberg@angelaschule-osnabrueck.net>
2020-10-07 23:17:46 +0200

    Restructured and changed cell and sheet handling.

--- main.go
+++ main.go
@@ -34,44 +34,54 @@ func readPdf(path string) (string, error) {
 		if p.V.IsNull() {
 			continue
 		}
-		// Create a new sheet.
-		sheet := "Sheet" + fmt.Sprintf("%d", pageIndex)
-		x.NewSheet(sheet)
-		x.SetColWidth(sheet, "B", "B", 30)
-		x.SetColWidth(sheet, "G", "G", 30)
 
 		rows, _ := p.GetTextByRow()
 		//fmt.Printf("%v", rows)
 		for _, row := range rows {
 			println(">>>> row: ", row.Position)
-			// Set value of a cell.
-			x.SetCellValue(sheet, "B1", "Name")
-			x.SetCellValue(sheet, "C1", "Hj. 1")
-			x.SetCellValue(sheet, "D1", "Hj. 2")
-			x.SetCellValue(sheet, "E1", "Hj. 3")
-			x.SetCellValue(sheet, "F1", "Hj. 4")
-			x.SetCellValue(sheet, "G1", "Bemerkungen")
-
-			var i = 0
-			var j = 1
-			for _, word := range row.Content {
-				//fmt.Println(word.S)
-				i++
-				//if i < 30 {
-				//	continue
-				//}
+			if row.Content.Len() > 27 {
 				// Set value of a cell.
-				if i > 27 && i%4 != 0 && i%2 == 0 {
-					x.SetCellValue(sheet, "A"+fmt.Sprintf("%d", 1+j), j)
-					x.SetCellValue(sheet, "B"+fmt.Sprintf("%d", 1+j), word.S)
-					fmt.Println(word.S)
-					j++
+				// Create a new sheet with name of "kurs"
+				sheet := row.Content[9].S
+				x.NewSheet(sheet)
+				x.SetColWidth(sheet, "B", "B", 30)
+				x.SetColWidth(sheet, "G", "G", 30)
+				// Name
+				x.SetCellValue(sheet, "B1", row.Content[17].S)
+				// Hj. 1
+				x.SetCellValue(sheet, "C1", row.Content[19].S)
+				// Hj. 2
+				x.SetCellValue(sheet, "D1", row.Content[21].S)
+				// Hj. 3
+				x.SetCellValue(sheet, "E1", row.Content[23].S)
+				// Hj. 4
+				x.SetCellValue(sheet, "F1", row.Content[25].S)
+				// Bemerkungen
+				x.SetCellValue(sheet, "G1", row.Content[27].S)
+
+				var i = 0
+				var j = 1
+				for _, word := range row.Content {
+					//fmt.Println(word.S)
+					i++
+					//if i < 30 {
+					//	continue
+					//}
+					// Set value of a cell.
+					if i > 27 && i%4 != 0 && i%2 == 0 {
+						x.SetCellValue(sheet, "A"+fmt.Sprintf("%d", 1+j), j)
+						x.SetCellValue(sheet, "B"+fmt.Sprintf("%d", 1+j), word.S)
+						fmt.Println(word.S)
+						j++
+					}
 				}
 			}
 		}
 	}
+	// Delete standard sheet
+	x.DeleteSheet("Sheet1")
 	// Set active sheet of the workbook.
-	x.SetActiveSheet(0)
+	x.SetActiveSheet(2)
 	// Save xlsx file by the given path.
 	if err := x.SaveAs("AbiKursliste.xlsx"); err != nil {
 		fmt.Println(err)
